041dd0cd36ae6b077e406df6d78205a86cd6d5a7
Hadiat Abdul Bashit <hadiatab16@gmail.com>
2023-11-15 10:53:51 +0700

    add ejs and views

--- weekTwo/express_server.js
+++ weekTwo/express_server.js
@@ -4,16 +4,33 @@ const path = require('path');
 const app = express()
 const port = 3000
 
+
+const contacts = [
+    {
+        nama: 'hadiat', 
+        telp: '08231827612'
+    },
+    {
+        nama: 'abdul',
+        telp: '08123782131'
+    },
+    {
+        nama: 'bashit',
+        telp: '06132873921'
+    }
+]
+
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/index.html'));
 })
 app.get('/about', (req, res) => {
-    res.render('about')
+    res.render('about', {nama: 'Hadiat Abdul', title: 'About'})
 })
 app.get('/contact', (req, res) => {
-    res.sendFile('./views/contact.html', {root: __dirname})
+    // res.sendFile('./views/contact.html', {root: __dirname})
+    res.render('contact', {contacts})
 })
 app.get('/product/:id', (req, res) => {
     res.send('<h1>product id: ' + req.params.id + '<br>Category: ' + req.query.category + '<h1>')
