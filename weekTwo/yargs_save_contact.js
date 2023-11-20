bea1bd2bb1667b9126c1c8843e7470962f87e27d
Hadiat Abdul Bashit <hadiatab16@gmail.com>
2023-11-20 09:29:41 +0700

    Menambahakn komentar untuk dokumentasi

--- weekTwo/yargs_save_contact.js
+++ weekTwo/yargs_save_contact.js
@@ -1,8 +1,11 @@
+// Memuat modul yargs untuk memproses argumen baris perintah
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
 
+// Memuat modul eksternal saveContact untuk menyimpan informasi kontak
 const saveContact = require('./save_contact');
 
+// Mengolah argumen baris perintah menggunakan yargs
 const argv = yargs(hideBin(process.argv))
   .option('name', {
     alias: 'n',
@@ -13,7 +16,7 @@ const argv = yargs(hideBin(process.argv))
   .option('phone', {
     alias: 'p',
     type: 'string',
-    description: 'input your phone number',
+    description: 'Input your phone number',
     demandOption: true
   })
   .option('email', {
@@ -24,8 +27,10 @@ const argv = yargs(hideBin(process.argv))
   .help()
   .argv;
 
+// Menampilkan informasi kontak dari argumen baris perintah
 console.log(`Name         : ${argv.name}`);
 console.log(`Phone Number : ${argv.phone}`);
 console.log(`Email        : ${argv.email}`);
 
+// Memanggil fungsi saveContact untuk menyimpan informasi kontak
 saveContact(argv.name, argv.phone, argv.email);
