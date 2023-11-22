9c3007402da36759c43436398aa1313193856229
Hadiat Abdul Bashit <hadiatab16@gmail.com>
2023-11-22 10:30:03 +0700

    add semantic ui with cdn and add card in content

--- weekThree/react-app/src/App.js
+++ weekThree/react-app/src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
 
 function App() {
-  return <input type='number' min={5} style={{ border: '3px solid' }} />
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Hadiat
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 8.00AM</span>
+          </div>
+          <div className="text">test Comment</div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App;
