8c648ae409a998b89cb915ab87f731ca132ea20c
Hadiat Abdul Bashit <hadiatab16@gmail.com>
2023-11-22 09:59:21 +0700

    change app to input

--- weekThree/react-app/src/App.js
+++ weekThree/react-app/src/App.js
@@ -1,22 +1,7 @@
 import "./App.css";
 
-const identitas = (nama, pekerjaan) => {
-  return (
-    <>
-      <h1>Hai nama saya {nama}</h1>
-      <h2>Pekerjaan saya {pekerjaan}</h2>
-    </>
-  )
-}
-
 function App() {
-  return (
-    <>
-      <h1>{new Date().toLocaleTimeString()}</h1>
-      {identitas('Hadiat', 'Tidur')}
-      {identitas('Abdul', 'Makan')}
-    </>
-  )
+  return <input type='number' min={5} style={{ border: '3px solid' }} />
 }
 
 export default App;
