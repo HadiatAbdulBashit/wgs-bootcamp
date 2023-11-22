07a52c5a89fdf077d94b3992edc97aa0cc45ef14
Hadiat Abdul Bashit <hadiatab16@gmail.com>
2023-11-22 10:50:18 +0700

    use faker for content in comment

--- weekThree/react-app/src/App.js
+++ weekThree/react-app/src/App.js
@@ -1,20 +1,27 @@
+import { faker } from '@faker-js/faker';
 import "./App.css";
+
+const randomName = faker.person.fullName();
+const randomAvatar = faker.image.avatar();
+const randomTime = faker.date.recent().toLocaleTimeString();
+const randomComment = faker.lorem.paragraph();
+console.log(randomTime);
 
 function App() {
   return (
     <div className="ui container comments">
       <div className="comment">
         <a href="/" className="avatar">
-          <img alt="avatar" />
+          <img src={randomAvatar} alt="avatar" />
         </a>
         <div className="content">
           <a href="/" className="author">
-            Hadiat
+            {randomName}
           </a>
           <div className="metadata">
-            <span className="date">Today at 8.00AM</span>
+            <span className="date">{randomTime}</span>
           </div>
-          <div className="text">test Comment</div>
+          <div className="text">{randomComment}</div>
         </div>
       </div>
     </div>
