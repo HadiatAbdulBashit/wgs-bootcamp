27feba9277f6d512a6df1a6d3de984fcfd359a07
Hadiat Abdul Bashit <hadiatab16@gmail.com>
2023-11-22 15:48:27 +0700

    add objek for rendering

--- weekThree/react-app/src/App.js
+++ weekThree/react-app/src/App.js
@@ -5,23 +5,29 @@ const randomName = faker.person.fullName();
 const randomAvatar = faker.image.avatar();
 const randomTime = faker.date.recent().toLocaleTimeString();
 const randomComment = faker.lorem.paragraph();
-console.log(randomTime);
+
+const data = {
+  name: randomName,
+  avatar: randomAvatar,
+  time: randomTime,
+  comment: randomComment
+}
 
 function App() {
   return (
     <div className="ui container comments">
       <div className="comment">
         <a href="/" className="avatar">
-          <img src={randomAvatar} alt="avatar" />
+          <img src={data.avatar} alt="avatar" />
         </a>
         <div className="content">
           <a href="/" className="author">
-            {randomName}
+            {data.name}
           </a>
           <div className="metadata">
-            <span className="date">{randomTime}</span>
+            <span className="date">{data.time}</span>
           </div>
-          <div className="text">{randomComment}</div>
+          <div className="text">{data.comment}</div>
         </div>
       </div>
     </div>
