b0fed60a67cbcdd6e5c8b6d1e8c8170d338a3c73
Hadiat Abdul Bashit <hadiatab16@gmail.com>
2023-11-22 09:51:09 +0700

    add call function 2x

--- weekThree/react-app/src/App.js
+++ weekThree/react-app/src/App.js
@@ -1,8 +1,22 @@
-// import logo from "./logo.svg";
 import "./App.css";
 
+const identitas = (nama, pekerjaan) => {
+  return (
+    <>
+      <h1>Hai nama saya {nama}</h1>
+      <h2>Pekerjaan saya {pekerjaan}</h2>
+    </>
+  )
+}
+
 function App() {
-  return new Date().toLocaleTimeString()
+  return (
+    <>
+      <h1>{new Date().toLocaleTimeString()}</h1>
+      {identitas('Hadiat', 'Tidur')}
+      {identitas('Abdul', 'Makan')}
+    </>
+  )
 }
 
 export default App;
