5fdd6ee11287aaded9e01179e394bbdb1719bcbd
Hadiat Abdul Bashit <hadiatab16@gmail.com>
2023-12-01 14:45:39 +0700

    fix change video when click thumbail

--- weekFour/react-app/src/component/VideoThumbnail.js
+++ weekFour/react-app/src/component/VideoThumbnail.js
@@ -2,10 +2,10 @@ function VideoThumbnail({ videos, setSelectedVideo }) {
     return (
         <div className="ui items segment">
             {videos.slice(1).map((video) => (
-                <div key={video.id.videoId} className='item'>
-                    <img src={video.snippet.thumbnails.default.url} alt={video.snippet.title} className="ui small image" onClick={setSelectedVideo(video)} />
+                <div key={video.id.videoId} className='item' onClick={() => setSelectedVideo(video)} style={{ cursor: 'pointer' }}>
+                    <img src={video.snippet.thumbnails.default.url} alt={video.snippet.title} className="ui small image"/>
                     <div className="content">
-                        <h4 onClick={setSelectedVideo(video)} >{video.snippet.title}</h4>
+                        <h4>{video.snippet.title}</h4>
                         <p>By: {video.snippet.channelTitle || 'Unknow'}</p>
                         <a href={`https://www.youtube.com/embed/${video.id.videoId}`}>See on Youtube</a>
                     </div>
