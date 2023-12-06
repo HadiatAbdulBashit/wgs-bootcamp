2494a7f475abc802c502ffc354fb7bd21141bc79
Hadiat Abdul Bashit <hadiatab16@gmail.com>
2023-12-06 11:17:31 +0700

    add styling

--- weekFour/react-app/src/component/VideoThumbnail.js
+++ weekFour/react-app/src/component/VideoThumbnail.js
@@ -3,7 +3,7 @@ function VideoThumbnail({ videos, setSelectedVideo }) {
         <div className="ui items segment">
             {videos.slice(1).map((video) => (
                 <div key={video.id.videoId} className='item' onClick={() => setSelectedVideo(video)} style={{ cursor: 'pointer' }}>
-                    <img src={video.snippet.thumbnails.default.url} alt={video.snippet.title} className="ui small image"/>
+                    <img src={video.snippet.thumbnails.default.url} alt={video.snippet.title} className="ui tiny rounded image"/>
                     <div className="content">
                         <h4>{video.snippet.title}</h4>
                         <p>By: {video.snippet.channelTitle || 'Unknow'}</p>
